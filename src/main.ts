18f8dfa8889a11532b2c799c95d963cc83c6690a
sugyan <sugi1982@gmail.com>
2021-09-21 01:08:01 +0900

    Use touchEvent, object classes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
 import p5 from "p5";
 
 const sketch = (p: p5) => {
-  p.setup = () => {
-    p.createCanvas(400, 400);
-  };
+  class Circle {
+    private x: number;
+    private y: number;
+    private size = 50;
+    public constructor(x: number, y: number) {
+      this.x = x;
+      this.y = y;
+    }
+    public display() {
+      p.ellipse(this.x, this.y, this.size, this.size);
+    }
+  }
+  const circles: Circle[] = [];
 
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+  };
   p.draw = () => {
-    p.background(220);
-    p.ellipse(50, 50, 80, 80);
+    circles.forEach((circle) => circle.display());
+  };
+  p.touchMoved = () => {
+    circles.push(new Circle(p.mouseX, p.mouseY));
+    return false;
   };
 };
 
