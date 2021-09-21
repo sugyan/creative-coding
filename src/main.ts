8f1f8545fb3da29dcd51f713ce04501ceb246e36
sugyan <sugi1982@gmail.com>
2021-09-22 00:47:17 +0900

    Update colors

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,25 @@ const sketch = (p: p5) => {
     private x: number;
     private y: number;
     private created: number;
+    private color: string;
     private size = 50;
     private live = 3000;
     public constructor(x: number, y: number) {
       this.x = x;
       this.y = y;
       this.created = p.millis();
+      this.color = p
+        .colorMode(p.HSB)
+        .color((this.created / 10) % 360, 100, 100)
+        .toString();
     }
     public isLive(millis: number): boolean {
       return millis - this.created < this.live;
     }
     public draw(millis: number) {
       const score = 1.0 - (millis - this.created) / this.live;
-      p.stroke(0, 0, 0, score * 255.0);
+      p.stroke(0, 0, 0, score);
+      p.fill(this.color);
       p.ellipse(this.x, this.y, this.size * score, this.size * score);
     }
   }
@@ -30,7 +36,7 @@ const sketch = (p: p5) => {
   p.draw = () => {
     const now = p.millis();
     circles = circles.filter((circle) => circle.isLive(now));
-    p.clear();
+    p.background(0, 0, 0);
     circles.forEach((circle) => circle.draw(now));
   };
   p.touchMoved = () => {
