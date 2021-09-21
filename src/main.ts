f00e6aea4d810b290bee59d01ec033fc7bcc0892
sugyan <sugi1982@gmail.com>
2021-09-22 00:24:04 +0900

    Add lifetimes

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,34 @@ const sketch = (p: p5) => {
   class Circle {
     private x: number;
     private y: number;
+    private created: number;
     private size = 50;
+    private live = 3000;
     public constructor(x: number, y: number) {
       this.x = x;
       this.y = y;
+      this.created = p.millis();
     }
-    public display() {
-      p.ellipse(this.x, this.y, this.size, this.size);
+    public isLive(millis: number): boolean {
+      return millis - this.created < this.live;
+    }
+    public draw(millis: number) {
+      const score = 1.0 - (millis - this.created) / this.live;
+      p.stroke(0, 0, 0, score * 255.0);
+      p.ellipse(this.x, this.y, this.size * score, this.size * score);
     }
   }
-  const circles: Circle[] = [];
+
+  let circles: Circle[] = [];
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
   p.draw = () => {
-    circles.forEach((circle) => circle.display());
+    const now = p.millis();
+    circles = circles.filter((circle) => circle.isLive(now));
+    p.clear();
+    circles.forEach((circle) => circle.draw(now));
   };
   p.touchMoved = () => {
     circles.push(new Circle(p.mouseX, p.mouseY));
