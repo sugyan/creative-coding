f58f69f4b6360147550b516fc5a4a00de42335c1
sugyan <sugi1982@gmail.com>
2021-10-03 00:16:31 +0900

    Update p5-wrapper

--- src/components/p5-wrapper.tsx
+++ src/components/p5-wrapper.tsx
@@ -1,7 +1,29 @@
-import { ReactP5Wrapper } from "react-p5-wrapper";
+import p5 from "p5";
+import React, { FC, useEffect, useRef, useState } from "react";
 
-const P5Wrapper = ({ name }) => {
-  const sketch = require(`../sketches/${name}`).default;
-  return <ReactP5Wrapper sketch={sketch} />;
+export interface Sketch {
+  (instance: p5): void;
+}
+
+export interface P5WrapperProps {
+  name: string;
+}
+
+const ReactP5WrapperComponent: FC<P5WrapperProps> = ({ name }) => {
+  const wrapper = useRef<HTMLDivElement>();
+  const [_, setInstance] = useState<p5>();
+
+  useEffect(() => {
+    if (wrapper.current === null) return;
+    const sketch = require(`../sketches/${name}`).default;
+    const canvas = new p5(sketch, wrapper.current);
+    setInstance(canvas);
+    return () => {
+      canvas.remove();
+    };
+  }, [name]);
+
+  return <div ref={wrapper} />;
 };
-export default P5Wrapper;
+
+export default ReactP5WrapperComponent;
