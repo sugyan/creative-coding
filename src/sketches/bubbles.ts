fe217daaf3630f8ca7a2d163a07e8aaa77397ed6
sugyan <sugi1982@gmail.com>
2021-10-05 01:04:28 +0900

    Update bubbles

--- src/sketches/bubbles.ts
+++ src/sketches/bubbles.ts
@@ -2,59 +2,71 @@ import p5 from "p5";
 
 const sketch = (p: p5) => {
   class Bubble {
-    private readonly baseX: number;
+    public readonly created: number;
     private x: number;
     private y: number;
+    private dx: number;
+    private dy: number;
+    private readonly ddy: number;
     private radius: number;
     public constructor() {
-      const radiusMin = p.windowWidth * (1 / 10 - 1 / 40);
-      const radiusMax = p.windowWidth * (1 / 10 + 1 / 40);
+      const [h, w] = [p.windowHeight, p.windowWidth];
+      this.created = p.millis();
+      const radiusMin = p.max(h, w) * (1 / 10 - 1 / 40);
+      const radiusMax = p.max(h, w) * (1 / 10 + 1 / 40);
       this.radius = p.random(radiusMin, radiusMax);
-      this.x = this.baseX = p.random(0, p.windowWidth);
-      this.y = p.windowHeight + this.radius * p.random(0, 5);
+      this.x = 0;
+      this.y = h;
+      const angle = p.random(0, (p.PI / 2) * 0.9);
+      const hypot = p.sqrt(h * h + w * w);
+      this.dx = (p.cos(angle) * hypot) / 300;
+      this.dy = (p.sin(angle) * hypot) / 300;
+      this.ddy = this.dx / 1000;
     }
     public move() {
-      const noise = p.noise(this.baseX + p.millis() / 2500.0);
-      this.x = this.baseX + (noise - 0.5) * this.radius;
-      this.y -= (p.windowHeight / 300) * (1 + noise);
+      const noise = p.noise(this.created + p.millis());
+      this.x += this.dx;
+      this.y -= this.dy;
+      this.dx += noise / 1000;
+      this.dy += this.ddy + noise / 1000;
     }
     public draw() {
       const millis = p.millis();
-      const noiseX = p.noise(this.baseX + millis / 300.0 + 0);
-      const noiseY = p.noise(this.baseX + millis / 300.0 + 1);
-      const noiseA = p.noise(this.baseX + millis / 300.0 + 2);
+      const noiseX = p.noise(this.created + 0 + millis / 300);
+      const noiseY = p.noise(this.created + 1 + millis / 300);
+      const noiseA = p.noise(this.created + 2 + millis / 300);
       p.stroke(0xb0, 0xb0, 0xb0);
-      p.fill(0xff, 0xff, 0xff, 0x40);
+      p.fill(0xff, 0xff, 0xff, 0x60);
 
       p.push();
       p.translate(this.x, this.y);
       // body
       p.push();
-      p.rotate((noiseA * 2 - 1.0) * p.PI);
+      p.rotate((noiseA * 2 - 1) * p.PI);
       p.ellipse(
         0,
         0,
-        this.radius * (1 + 0.2 * (noiseX - 0.5)),
-        this.radius * (1 + 0.2 * (noiseY - 0.5))
+        this.radius * (1 + 0.3 * (noiseX - 0.5)),
+        this.radius * (1 + 0.3 * (noiseY - 0.5))
       );
       p.pop();
       // reflection of light
-      p.translate(-this.radius * 0.225, -this.radius * 0.225);
-      p.rotate((noiseA * 2 - 1.0) * p.PI);
+      p.translate(-this.radius * 0.2, -this.radius * 0.2);
+      p.rotate((p.PI / 4) * (0.5 + noiseA));
       p.noStroke();
       Array.from(Array(5).keys()).forEach((i) => {
         p.fill(0xff, 0xff, 0xff, 0x3f + 0xc0 * (1 / 10) * i);
         p.ellipse(
           0,
           0,
-          this.radius * (0.2 - 0.2 * (1 / 10) * i + 0.075 * (noiseX - 0.5)),
-          this.radius * (0.2 - 0.2 * (1 / 10) * i + 0.075 * (noiseY - 0.5))
+          this.radius * (0.2 - 0.2 * (1 / 10) * i + 0.1 * (noiseX - 0.5)),
+          this.radius * (0.3 - 0.3 * (1 / 10) * i + 0.1 * (noiseY - 0.5))
         );
       });
       p.pop();
     }
     public isLive(): boolean {
-      return this.y + this.radius >= 0;
+      return this.x - this.radius <= p.windowWidth && this.y + this.radius >= 0;
     }
   }
 
@@ -76,10 +88,13 @@ const sketch = (p: p5) => {
       bubble.move();
       bubble.draw();
     });
-    if (bubbles.length < 10) {
-      bubbles.push(new Bubble());
-    }
     bubbles = bubbles.filter((bubble) => bubble.isLive());
+    if (bubbles.length < 12) {
+      const latest = bubbles[bubbles.length - 1];
+      if (!latest || p.millis() - latest.created > 150) {
+        bubbles.push(new Bubble());
+      }
+    }
   };
   p.windowResized = () => {
     p.resizeCanvas(p.windowWidth, p.windowHeight);
