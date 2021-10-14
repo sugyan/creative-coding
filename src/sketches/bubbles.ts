069559ed3efcbfd1b091962569c7f0271264d089
sugyan <sugi1982@gmail.com>
2021-10-15 00:15:31 +0900

    Update bubbles

--- src/sketches/bubbles.ts
+++ src/sketches/bubbles.ts
@@ -5,60 +5,37 @@ const sketch = (p: p5) => {
     public readonly created: number;
     public x: number;
     public y: number;
-    public size: number;
-    public broken?: number;
+    public hue: number;
+    public readonly size: number;
     private t = 0;
-    private dx: number;
-    private dy: number;
+    private readonly dx: number;
+    private readonly dy: number;
     private readonly ddy: number;
     public constructor() {
-      const [h, w] = [p.windowHeight, p.windowWidth];
       this.created = p.millis();
+      const [h, w] = [p.windowHeight, p.windowWidth];
       const sizeMin = p.max(h, w) * (1 / 10 - 1 / 40);
       const sizeMax = p.max(h, w) * (1 / 10 + 1 / 40);
       this.size = p.random(sizeMin, sizeMax);
       this.x = 0;
       this.y = h;
       const angle = (p.PI / 2) * (p.random() * 0.9) ** 1.5;
-      const hypot = p.sqrt(h * h + w * w);
-      this.dx = (p.cos(angle) * hypot) / 300;
-      this.dy = (p.sin(angle) * hypot) / 300;
+      const v = (p.sqrt(h * h + w * w) * p.max(h, w)) / 4000 / this.size;
+      this.dx = p.cos(angle) * v;
+      this.dy = p.sin(angle) * v;
       this.ddy = (this.dx / w) * 2;
     }
-    public move() {
-      this.t++;
-      if (p.mouseIsPressed || p.touches.length === 1) {
-        const [x, y] = [p.mouseX, p.mouseY];
-        if (p.sqrt((this.x - x) ** 2 + (this.y - y) ** 2) < this.size / 2) {
-          this.broken = p.millis();
-        }
-      }
-      if (!this.broken) {
-        const h = p.windowHeight;
-        const noise = p.noise(this.created + p.millis() / 1000);
-        this.x = 0 + this.dx * this.t + ((noise - 0.5) * this.size) / 5;
-        this.y = h - this.dy * this.t + ((noise - 0.5) * this.size) / 5;
-        this.y -= (this.ddy * this.t * this.t) / 2;
-      }
-    }
     public draw() {
-      if (this.broken) {
-        const rate = 1 + (p.millis() - this.broken) / 200;
-        const n = p.noise(this.t / 300, this.size);
-        const hue = (n * 480 + 300) % 360;
-        p.strokeWeight(7);
-        for (let i = 0; i < p.max(10, this.size / 10); i++) {
-          p.stroke(p.colorMode(p.HSB).color(hue, 100, 100, 0.4 - 0.02 * i));
-          const a = p.noise(this.created + i) * 20 * p.PI;
-          p.point(
-            this.x + (this.size / 2) * p.cos(a) * rate,
-            this.y + (this.size / 2) * p.sin(a) * rate
-          );
-        }
-        return;
-      }
-
+      // move
+      this.t++;
       const millis = p.millis();
+      const h = p.windowHeight;
+      const n = p.noise(this.created + millis / 1000) - 0.5;
+      this.x = 0 + this.dx * this.t + (n * this.size) / 5;
+      this.y = h - this.dy * this.t + (n * this.size) / 5;
+      this.y -= (this.ddy * this.t * this.t) / 2;
+      this.hue = (p.noise(this.t / 300, this.size) * 480 + 300) % 360;
+      // draw
       const noiseX = p.noise(this.created + 0 + millis / 300);
       const noiseY = p.noise(this.created + 1 + millis / 300);
       const noiseA = p.noise(this.created + 2 + millis / 300);
@@ -68,13 +45,11 @@ const sketch = (p: p5) => {
       p.push();
       p.rotate((noiseA * 2 - 1) * p.PI);
       const weight = this.size / 25;
-      p.fill(p.colorMode(p.RGB).color(0xff, 0xff, 0xff, 0x10));
+      p.fill(p.colorMode(p.RGB).color(0xff, 0xff, 0xff, 0x08));
       p.strokeWeight(weight);
       p.colorMode(p.HSB);
       for (let i = 0; i <= 15; i++) {
-        const n = p.noise(this.t / 300, this.size + i / 50);
-        const hue = (n * 480 + 300) % 360;
-        p.stroke(p.color(hue, 100, 100, 0.2 - i / 30));
+        p.stroke(p.color(this.hue, 100, 100, 0.2 - i / 30));
         p.ellipse(
           0,
           0,
@@ -82,14 +57,6 @@ const sketch = (p: p5) => {
           this.size * (1 + 0.3 * (noiseY - 0.5)) - i * weight * 2
         );
       }
-      p.strokeWeight(1);
-      p.stroke(p.colorMode(p.RGB).color(0x80, 0x80, 0x80, 0x20));
-      p.ellipse(
-        0,
-        0,
-        this.size * (1 + 0.3 * (noiseX - 0.5)),
-        this.size * (1 + 0.3 * (noiseY - 0.5))
-      );
       p.pop();
       // reflection of light
       p.translate(-this.size * 0.2, -this.size * 0.2);
@@ -108,40 +75,82 @@ const sketch = (p: p5) => {
       p.pop();
     }
     public isLive(): boolean {
-      return (
-        (!this.broken || p.millis() - this.broken < 100) &&
-        this.x - this.size <= p.windowWidth &&
-        this.y + this.size >= 0
+      return this.x - this.size <= p.windowWidth && this.y + this.size >= 0;
+    }
+  }
+  class Drops {
+    private readonly created: number;
+    private readonly x: number;
+    private readonly y: number;
+    private readonly hue: number;
+    private readonly size: number;
+    private readonly points: Array<[number, number]>;
+    constructor(bubble: Bubble) {
+      this.created = p.millis();
+      this.x = bubble.x;
+      this.y = bubble.y;
+      this.hue = bubble.hue;
+      this.size = bubble.size;
+      this.points = Array.from(
+        Array(p.floor((this.size * this.size) / 250)),
+        () => {
+          const z = p.random(-1, 1);
+          const s = p.sqrt(1 - z * z) / 2;
+          const phi = p.random(0, 2 * p.PI);
+          return [
+            this.x + this.size * s * p.cos(phi),
+            this.y + this.size * s * p.sin(phi),
+          ];
+        }
       );
+    }
+    public draw() {
+      const t = p.millis() - this.created;
+      const alpha = 0.75 * (1 - t / 500);
+      p.stroke(p.colorMode(p.HSB).color(this.hue, 100, 100, alpha));
+      p.strokeWeight(3);
+      this.points.forEach((pos) => p.point(pos[0], pos[1] + (t * t) / 20000));
+    }
+    public isLive(): boolean {
+      return p.millis() - this.created < 500;
     }
   }
 
   let bubbles: Bubble[] = [];
+  let drops: Drops[] = [];
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
   p.draw = () => {
+    const millis = p.millis();
     // background
-    const h = p.windowHeight;
-    const c0 = p.colorMode(p.RGB).color(0x00, 0xff, 0xff);
-    const c1 = p.colorMode(p.RGB).color(0xb0, 0xff, 0xff);
-    for (let i = 0; i <= h; i++) {
-      p.stroke(p.lerpColor(c0, c1, p.map(i, 0, p.windowHeight, 0, 1)));
-      p.line(0, i, p.windowWidth, i);
-    }
+    p.background(p.colorMode(p.RGB).color(0x80, 0x80, 0x80));
     // bubbles
-    bubbles.forEach((bubble: Bubble) => {
-      bubble.move();
-      bubble.draw();
+    bubbles = bubbles.filter((bubble: Bubble) => {
+      if (!bubble.isLive()) {
+        return false;
+      }
+      if (p.mouseIsPressed || p.touches.length === 1) {
+        const [x, y] = [p.mouseX, p.mouseY];
+        const d = p.sqrt((bubble.x - x) ** 2 + (bubble.y - y) ** 2);
+        if (d < bubble.size / 2 + 10 && millis - bubble.created > 500) {
+          drops.push(new Drops(bubble));
+          return false;
+        }
+      }
+      return true;
     });
-    bubbles = bubbles.filter((bubble) => bubble.isLive());
+    bubbles.forEach((bubble: Bubble) => bubble.draw());
     if (bubbles.length < 12) {
       const latest = bubbles[bubbles.length - 1];
-      if (!latest || p.millis() - latest.created > 150) {
+      if (!latest || millis - latest.created > 200) {
         bubbles.push(new Bubble());
       }
     }
+    // drops
+    drops = drops.filter((d: Drops) => d.isLive());
+    drops.forEach((d: Drops) => d.draw());
   };
   p.touchMoved = () => {
     return false;
