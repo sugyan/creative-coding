da2793539e1deba69a2367df996a72a5aff73610
sugyan <sugi1982@gmail.com>
2021-10-24 16:46:19 +0900

    Update hexagon

--- src/sketches/hexagon.ts
+++ src/sketches/hexagon.ts
@@ -8,7 +8,7 @@ interface Point {
 const sketch = (p: p5) => {
   const num = 2000;
   const hsq3 = p.sqrt(3) / 2;
-  let grid: number[][] = [];
+  let grid: boolean[][] = [];
   let size: number;
 
   const resize = (): void => {
@@ -17,9 +17,9 @@ const sketch = (p: p5) => {
 
     const rows = p.ceil(h / size / hsq3) + 1;
     const cols = p.ceil(w / size);
-    grid = Array.from(Array(rows), () => Array.from(Array(cols), () => 0));
-    for (let i = 0; i < num / 5; i++) {
-      grid[p.floor(p.random(rows))][p.floor(p.random(cols))] = 1;
+    grid = Array.from(Array(rows), () => Array.from(Array(cols), () => false));
+    for (let i = 0; i < num / 10; i++) {
+      grid[p.floor(p.random(rows))][p.floor(p.random(cols))] = true;
     }
   };
   const drawer = (size: number): ((x: number, y: number) => void) => {
@@ -43,7 +43,7 @@ const sketch = (p: p5) => {
     );
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
-        if (grid[i][j] == 0) continue;
+        if (!grid[i][j]) continue;
         if (j > 0) neighbors[i][j - 1]++;
         if (j < cols - 1) neighbors[i][j + 1]++;
         const k = j - 1 + (i & 1);
@@ -60,38 +60,41 @@ const sketch = (p: p5) => {
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
         const n = neighbors[i][j];
-        if (
-          (grid[i][j] > 0 && (n == 3 || n == 5)) ||
-          (grid[i][j] == 0 && (n == 2 || n == 4))
-        ) {
-          grid[i][j] = 1;
-        } else {
-          grid[i][j] = 0;
-        }
-        if (p.random() < 1 / num) {
-          grid[i][j]++;
-        }
+        grid[i][j] =
+          (grid[i][j] && (n == 3 || n == 5)) ||
+          (!grid[i][j] && (n == 2 || n == 4));
       }
     }
+  };
+  const touch = (x: number, y: number): void => {
+    const [w, h] = [p.windowWidth, p.windowHeight];
+    if (x >= w || y >= h) {
+      return;
+    }
+    const rows = p.ceil(h / size / hsq3);
+    const cols = p.ceil(w / size);
+    grid[p.floor((y / h) * rows)][p.floor((x / w) * cols)] = true;
   };
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.frameRate(24);
     resize();
   };
   p.draw = () => {
     const rows = p.ceil(p.windowHeight / size / hsq3) + 1;
     const cols = p.ceil(p.windowWidth / size);
-    next(rows, cols);
-
+    if (p.frameCount % 5 === 0) {
+      next(rows, cols);
+    }
     p.background(0x00, 0x00, 0x00);
-    p.stroke(0x00, 0xff, 0xff);
+    p.stroke(0x80, 0x80, 0x80);
     const draw = drawer(size);
     for (let i = 0; i < rows; i++) {
-      const offset = i % 2 == 0 ? 0 : size / 2;
+      const offset = (size / 2) * (i & 1);
       for (let j = 0; j < cols; j++) {
         p.fill(
-          grid[i][j] > 0 ? p.color(0xff, 0x00, 0xff) : p.color(0x00, 0x00, 0x00)
+          grid[i][j] ? p.color(0x00, 0xff, 0x80) : p.color(0x00, 0x00, 0x00)
         );
         draw(offset + size * j, size * i * hsq3);
       }
@@ -101,7 +104,11 @@ const sketch = (p: p5) => {
     resize();
     p.resizeCanvas(p.windowWidth, p.windowHeight);
   };
+  p.mouseClicked = () => {
+    touch(p.mouseX, p.mouseY);
+  };
   p.touchMoved = () => {
+    touch(p.mouseX, p.mouseY);
     return false;
   };
 };
