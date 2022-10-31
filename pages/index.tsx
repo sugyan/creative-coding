06dd4ee7e699a2b42348664cfb2ee52c39ee7ea7
sugyan <sugi1982@gmail.com>
2022-10-31 22:38:08 +0900

    Fix links

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,30 +15,22 @@ export default function Home() {
         <div className={styles.grid}>
           <div className={styles.card}>
             <Link href="/sketches/ghost-drawing">
-              <a>
-                <h2>Ghost drawing</h2>
-              </a>
+              <h2>Ghost drawing</h2>
             </Link>
           </div>
           <div className={styles.card}>
             <Link href="/sketches/bubbles">
-              <a>
-                <h2>Bubbles</h2>
-              </a>
+              <h2>Bubbles</h2>
             </Link>
           </div>
           <div className={styles.card}>
             <Link href="/sketches/gasshan">
-              <a>
-                <h2>Gasshan</h2>
-              </a>
+              <h2>Gasshan</h2>
             </Link>
           </div>
           <div className={styles.card}>
             <Link href="/sketches/hexagonal-gameoflife">
-              <a>
-                <h2>Hexagonal Game-of-Life</h2>
-              </a>
+              <h2>Hexagonal Game-of-Life</h2>
             </Link>
           </div>
         </div>
