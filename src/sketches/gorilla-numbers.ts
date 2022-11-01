81c31f5f1056fab13afebb209ba9b5120941d505
sugyan <sugi1982@gmail.com>
2022-11-01 15:52:02 +0900

    Fix bounds

--- src/sketches/gorilla-numbers.ts
+++ src/sketches/gorilla-numbers.ts
@@ -1,68 +1,109 @@
 import p5 from "p5";
 
+interface Bound {
+  t: number;
+  l: number;
+  b: number;
+  r: number;
+}
+
 const sketch = (p: p5) => {
+  class State {
+    readonly target: number;
+    public constructor() {
+      this.target = 0;
+    }
+  }
   class Digit {
-    private x: number;
-    private y: number;
-    private num: number;
-    public constructor(num: number, xy: [number, number]) {
+    readonly num: number;
+    readonly x: number;
+    readonly y: number;
+    readonly bound: Bound;
+    public constructor(
+      num: number,
+      xy: [number, number],
+      wh: [number, number]
+    ) {
+      const bbox = font.textBounds(`${num}`, 0, 0, p.textSize());
+      const xOffset = (wh[0] - bbox["w"]) / 2.0;
+      const yOffset = (wh[1] - bbox["h"]) / 2.0;
       this.num = num;
-      this.x = xy[0];
-      this.y = xy[1];
+      this.x = xy[0] - bbox["x"];
+      this.y = xy[1] - bbox["y"] - bbox["h"];
+      this.bound = {
+        t: xy[1] - yOffset - bbox["h"],
+        l: xy[0] - xOffset,
+        b: xy[1] + yOffset,
+        r: xy[0] + xOffset + bbox["w"],
+      };
     }
     public draw() {
       p.fill(255, 255, 255);
-      p.textFont("monospace");
-      p.text(
-        `${this.num}`,
-        this.x + p.noise(this.x + p.millis() / 500) * 10,
-        this.y + p.noise(this.y + p.millis() / 500) * 10
-      );
+      p.noStroke();
+      p.text(`${this.num}`, this.x, this.y);
     }
-    public isHit(
-      [x, y]: [number, number],
-      [w, h]: [number, number],
-      s: number
-    ): boolean {
-      return p.abs(this.x - x) < w * s && p.abs(this.y - y) < h * s;
+    public isHit([x, y]: [number, number], scale: number = 0.0): boolean {
+      const w = this.bound.r - this.bound.l;
+      const h = this.bound.b - this.bound.t;
+      return (
+        this.bound.l - w * scale < x &&
+        x < this.bound.r + w * scale &&
+        this.bound.t - h * scale < y &&
+        y < this.bound.b + h * scale
+      );
     }
   }
   function reset() {
     const [w, h] = [p.width, p.height];
     const l = p.min(w, h);
     const textSize = l / 8.0;
+    const bbox = font.textBounds(" 0123456789 ", 0, 0, textSize);
+    const [bw, bh] = [bbox["w"] / 12.0, bbox["h"]];
     p.textSize(textSize);
     digits.splice(0, digits.length);
     [...Array(10).keys()].forEach((num) => {
-      console.log(`num: ${num}, font: ${font}`);
-      const bbox = font.textBounds(`${num}`, 0, 0, textSize);
-      const [bw, bh] = [bbox["w"], bbox["h"]];
       while (true) {
         const xoffset = l * 0.05 + (w - l) / 2.0;
         const yoffset = l * 0.05 + (h - l) / 2.0;
         const x = p.random(l * 0.9 - bw) + xoffset;
         const y = p.random(l * 0.9 - bh) + yoffset + bh;
-        if (digits.every((d) => !d.isHit([x, y], [bw, bh], 2.5))) {
-          digits.push(new Digit(num, [x, y]));
+        const digit = new Digit(num, [x, y], [bw, bh]);
+        const points = [
+          [digit.bound.l, digit.bound.t],
+          [digit.bound.l, digit.bound.b],
+          [digit.bound.r, digit.bound.t],
+          [digit.bound.r, digit.bound.b],
+        ];
+        if (
+          digits.every((d) => points.every(([x, y]) => !d.isHit([x, y], 0.9)))
+        ) {
+          digits.push(digit);
           return;
         }
       }
     });
   }
+  const state = new State();
+  const digits: Digit[] = [];
   let font: p5.Font;
-  const digits = [];
   p.preload = () => {
-    console.log("preload");
     font = p.loadFont("/assets/fonts/Courier New.ttf");
   };
   p.setup = () => {
-    console.log("setup");
     p.createCanvas(p.windowWidth, p.windowHeight);
+    p.textFont(font);
     reset();
   };
   p.draw = () => {
     p.background(0, 0, 0);
     digits.forEach((digit) => digit.draw());
+  };
+  p.mouseClicked = () => {
+    digits.forEach((d) => {
+      if (d.isHit([p.mouseX, p.mouseY], 0.1)) {
+        console.log(d.num);
+      }
+    });
   };
 };
 
